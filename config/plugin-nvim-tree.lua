69edc7a88c0a4c444e2ed7558ef7ffdeb4166cd2
Jeffrey Schwartz <jefftschwartz@gmail.com>
2022-03-21 07:59:09 -0400

    Close nvim-tree when opening file.

--- config/plugin-nvim-tree.lua
+++ config/plugin-nvim-tree.lua
@@ -110,7 +110,7 @@ require'nvim-tree'.setup { -- BEGIN_DEFAULT_OPTS
             global = false,
         },
         open_file = {
-            quit_on_open = false,
+            quit_on_open = true,
             resize_window = false,
             window_picker = {
                 enable = true,
