3d1f838122ffe0626a6721a0f3abc48b3f72264e
Jeffrey Schwartz <jefftschwartz@gmail.com>
2022-03-28 23:13:59 -0400

    Removed unused auto_close configuration property.

--- config/plugin-nvim-tree.lua
+++ config/plugin-nvim-tree.lua
@@ -6,7 +6,6 @@
 -- setup with all defaults
 -- each of these are documented in `:help nvim-tree.OPTION_NAME`
 require'nvim-tree'.setup { -- BEGIN_DEFAULT_OPTS
-    auto_close = true,
     auto_reload_on_write = true,
     disable_netrw = false,
     hide_root_folder = false,
