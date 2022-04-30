2c10872430f3d3d5fade8f0cc305f0e3e9f3f853
Jeffrey Schwartz <jefftschwartz@gmail.com>
2022-04-29 20:06:59 -0400

    More tweaking.

--- config/plugin-neogen.lua
+++ config/plugin-neogen.lua
@@ -1,9 +1,10 @@
-local neogen = require('neogen')
-
-neogen.setup {
+----
+-- Neogen
+-- See https://github.com/danymat/neogen#requirements for details.
+----
+require'neogen'.setup {
     enabled = true,             --if you want to disable Neogen
     input_after_comment = true, -- (default: true) automatic jump (with insert mode) on inserted annotation
-    -- jump_map = "<C-e>"       -- (DROPPED SUPPORT, see [here](#cycle-between-annotations) !) The keymap in order to jump in the annotation fields (in insert mode)
 }
 
 local opts = { noremap = true, silent = true }
