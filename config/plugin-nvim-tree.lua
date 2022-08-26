cbb570f1bfe15e6eddf647d5d7af501bac7b22c5
Jeffrey Schwartz <jefftschwartz@gmail.com>
2022-08-26 08:45:08 -0400

    Tweaking nvim-tree config.

--- config/plugin-nvim-tree.lua
+++ config/plugin-nvim-tree.lua
@@ -49,7 +49,7 @@ require("nvim-tree").setup { -- BEGIN_DEFAULT_OPTS
         highlight_opened_files = "none",
         root_folder_modifier = ":~",
         indent_markers = {
-            enable = false,
+            enable = true,
             icons = {
                 corner = "└",
                 edge = "│",
@@ -101,7 +101,7 @@ require("nvim-tree").setup { -- BEGIN_DEFAULT_OPTS
         auto_open = true,
     },
     update_focused_file = {
-        enable = false,
+        enable = true,
         update_root = false,
         ignore_list = {},
     },
