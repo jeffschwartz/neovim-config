f42e44fef5e5965735ed8f2489782ff2756645bf
Jeffrey Schwartz <jefftschwartz@gmail.com>
2022-09-03 11:46:35 -0400

    Updated formating keymapping.

--- config/lspconfig.lua
+++ config/lspconfig.lua
@@ -34,7 +34,7 @@ local on_attach = function(client, bufnr)
     vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
     vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
     vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-    vim.keymap.set('n', '<space>f', vim.lsp.buf.formatting, bufopts)
+    vim.keymap.set('n', '<space>f', vim.lsp.buf.format, bufopts)
 
     -- Replace virtual inline diagnostics with a floating window on hover
     vim.diagnostic.config({
